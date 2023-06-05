4b005db4820750b4206914dc19fe8a4e22d334d0
weijia_serendipity <1289939352@qq.com>
2023-06-05 21:23:53 +0800

    resources and password box

---
+++ Assets/Scripts/Stage1/CheckPassword.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPassword : MonoBehaviour
+{
+    public GameObject correctText;
+    public GameObject wrongText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnshowCorrectText();
+        UnshowWrongText();
+        transform.GetComponent<InputField>().onEndEdit.AddListener(End_Value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void End_Value(string input)
+    {
+        if(input == "2197")
+        {
+            correctText.SetActive(true);
+            Invoke("UnshowCorrectText", 1.0f);
+            Invoke("PasswordCorrect", 1.5f);
+        }
+        else
+        {
+            wrongText.SetActive(true);
+            Invoke("UnshowWrongText", 1.0f);
+        }
+    }
+    void UnshowCorrectText()
+    {
+        correctText.SetActive(false);
+    }
+    void UnshowWrongText()
+    {
+        wrongText.SetActive(false);
+    }
+    void PasswordCorrect()
+    {
+        //TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+}
