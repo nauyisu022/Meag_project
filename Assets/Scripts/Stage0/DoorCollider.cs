f60d6506edc22a079901d0bc5c9fbba56ed363f3
weijia_serendipity <1289939352@qq.com>
2023-06-08 00:12:03 +0800

    .

--- Assets/Scripts/Stage0/DoorCollider.cs
+++ Assets/Scripts/Stage0/DoorCollider.cs
@@ -2,22 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DoorCollider : SceneBaseItem
 {
+    public GameObject text;
     public override void OnPointerClick(PointerEventData eventData)
     {
-        CameraManager.Instance.switchCamera("教室柜子Camera");
+
+        //这把钥匙似乎正好…（成功打开柜子）
+        if (ItemManager.Instance.objMap.ContainsKey("钥匙扣"))
+        {
+            text.SetActive(true);
+            text.GetComponent<Text>().text = "这把钥匙似乎正好…（成功打开柜子）";
+            Invoke("CloseText", 1.5f);
+            Invoke("TurnStage", 2.0f);
+        }
+
+        //带钥匙孔的铁制储物柜。无数个一模一样的柜子之一，上面的冰箱贴让它看起来特殊了一点。柜子被牢牢锁住了，还是不要强行破开它吧。
+        else
+        {
+            text.SetActive(true);
+            text.GetComponent<Text>().text = "带钥匙孔的铁制储物柜。无数个一模一样的柜子之一，上面的冰箱贴让它看起来特殊了一点。柜子被牢牢锁住了，还是不要强行破开它吧。";
+            Invoke("CloseText", 1.5f);
+        }
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void TurnStage()
+    {
+        CameraManager.Instance.switchCamera("教室柜子Camera");
+    }
+    void CloseText()
+    {
+        text.SetActive(false);
     }
 }
---
+++ Assets/Scripts/TriggerPart/PuzzleTrigger.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTrigger : BagBaseItem
+{
+    bool stage1to2 = false;
+    string _tag = null;
+    string _name = null;
+    string _description = null;
+    string checkText1 = null;
+    string checkText2 = null;
+    Sprite image1 = null;
+    Sprite image2 = null;
+    int curState = 1;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Init();
+        updateItemInfo(_tag, _name, _description, checkText1, image1, false);
+    }
+    void Init()
+    {
+        _tag = "拼图";
+        _name = "拼图";
+        _description = "拼图";
+
+        checkText1 = "如今已经很少见的老式拼图，不知道画着怎样的故事。";
+        checkText2 = "为了庆祝属于我们的纪念日，特意定制的拼图。专门去影楼拍了照片，一起精心挑选了好久。上面画着我们的故事。";
+        image1 = Resources.Load<Sprite>("柜子+拼图");
+        image2 = Resources.Load<Sprite>("柜子+拼图");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeState();
+    }
+    public void ChangeState()
+    {
+        //解开拼图：1-2
+        if (SceneManager.Instance.puzzleIsCorrect == true)
+        {
+            curState = 2;
+            if (stage1to2 == false)
+            {
+                updateItemInfo(_tag, _name, _description, checkText2, image2, false);
+                stage1to2 = true;
+            }
+
+        }
+
+    }
+}
