b18b9c71b2f957e22519228dbaa6a8a909302cbc
weijia_serendipity <1289939352@qq.com>
2023-06-08 11:44:45 +0800

    .

--- Assets/Scripts/Puzzle/Piece.cs
+++ Assets/Scripts/Puzzle/Piece.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
     // 拼图碎片类
-    public Vector3 initPosition; // 记录拼图的初始位置
     public Vector3 correctPosition; // 记录拼图的正确位置
     public Vector3 currentPosition; // 记录拼图的当前位置
 
-    // 定义一个阈值
-    float threshold = 0.3f;
-
     public bool IsCorrect()
     {
-        // 计算正确位置和当前位置之间的距离
-        float distance = Vector3.Distance(correctPosition, currentPosition);
-        // 如果距离小于或等于阈值，返回true，否则返回false
-        return distance <= threshold;
+        return correctPosition == currentPosition;
     }
 }
